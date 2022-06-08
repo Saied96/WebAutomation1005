337a0a85731186e8dc6b46253cf522a6e73a559e
email <saiddurrahman@gmail.com>
2022-06-07 21:17:56 -0400

    My WebAutomation 1005 project(google privacy)

---
+++ src/test/java/step_definitions/GoogleFooterLinks.java
@@ -0,0 +1,54 @@
+package step_definitions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class GoogleFooterLinks {
+
+	@Given("I go to Google Home page")
+	public void i_go_to_google_home_page() throws InterruptedException {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver",    "C:/Users/saied rahman/utilities/chromedriver.exe");            
+		WebDriver driver = new ChromeDriver();  // For Chrome Browser
+
+		driver.get("https://www.google.com");
+		Thread.sleep(2000);
+	}
+
+
+
+	@When("I click on the Privacy link")
+	public void i_click_on_the_privacy_link() throws InterruptedException {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver",    "C:/Users/saied rahman/utilities/chromedriver.exe");            
+		WebDriver driver = new ChromeDriver();  // For Chrome Browser
+
+		driver.get("https://www.google.com");
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Privacy")).click();
+		System.out.println("I clicked on the Privacy link ...");
+		Thread.sleep(2000);
+
+	}
+
+	@Then("I see the Privacy page")
+	public void i_see_the_privacy_page() throws InterruptedException {
+	    // Write code here that turns the phrase above into concrete actions
+		System.setProperty("webdriver.chrome.driver",    "C:/Users/saied rahman/utilities/chromedriver.exe");            
+		WebDriver driver = new ChromeDriver();  // For Chrome Browser
+
+		driver.get("https://www.google.com");
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("Privacy")).click();
+		System.out.println("I clicked on the Privacy link ...");
+		Thread.sleep(2000);
+		
+		
+	}
+
+}
